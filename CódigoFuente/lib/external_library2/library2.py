89d29da85ca0b72cc1b0c62506b7d6c4cc57bfda
Franko <150200472+Frankoxt@users.noreply.github.com>
2023-11-24 02:23:41 -0500

    Actualizar external_library2

---
+++ CódigoFuente/lib/external_library2/library2.py
@@ -0,0 +1,11 @@
+# library2.py
+
+class Library2Class:
+    def __init__(self, name):
+        self.name = name
+
+    def get_name(self):
+        """
+        Devuelve el nombre de la instancia de Library2Class.
+        """
+        return f"Nombre de la instancia: {self.name}"
