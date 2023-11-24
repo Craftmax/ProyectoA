124003dbe88fbf3c157ec3677f428f2e7c1bd02b
Franko <150200472+Frankoxt@users.noreply.github.com>
2023-11-24 02:20:49 -0500

    Actualizar external_library1

---
+++ CódigoFuente/lib/external_library1/library1.py
@@ -0,0 +1,7 @@
+# library1.py
+
+def some_function():
+    """
+    Realiza alguna operación en la biblioteca 1 y devuelve un resultado.
+    """
+    return "¡Resultado de some_function en external_library1!"
